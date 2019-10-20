2fb8f0b87562a6f36b9cda7816f4dbe944e852d2
okojyo <kyoukarada@gmail,com>
2019-10-20 13:20:13 +0900

    Introduced router

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const express = require('express')
 const app = express();
 const port = 3000;
+const router = express.Router();
 
-app.get('/', (req, res) => res.send('Hello World!'));
+router.get('/', (req, res) => res.send('Hello World!'));
+
+app.use('/', router);
 
 app.listen(port, () => console.log(`Example app listening on port ${port}`));
