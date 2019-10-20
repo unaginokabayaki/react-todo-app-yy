75bbfb22e51601d85048b3c9414fafc85e4d0611
okojyo <kyoukarada@gmail,com>
2019-10-20 13:29:22 +0900

    Added feature showing json data

--- app.js
+++ app.js
@@ -5,6 +5,26 @@ const router = express.Router();
 
 router.get('/', (req, res) => res.send('Hello World!'));
 
+router.get('/todos', (req, res) => {
+  res.json([
+    {
+      "_id": 1,
+      "name": "Item 1",
+      "done": false
+    },
+    {
+      "_id": 2,
+      "name": "Item 2",
+      "done": false
+    },
+    {
+      "_id": 3,
+      "name": "Item 3",
+      "done": false
+    }
+  ])
+})
+
 app.use('/', router);
 
 app.listen(port, () => console.log(`Example app listening on port ${port}`));
